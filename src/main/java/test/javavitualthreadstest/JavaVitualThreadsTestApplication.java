337bb216d489eeb8b83c95dc38bbc4229bfec8f0
hojinida <opqrs1103@gmail.com>
2024-05-20 13:31:28 +0900

    first commit

---
+++ src/main/java/test/javavitualthreadstest/JavaVitualThreadsTestApplication.java
@@ -0,0 +1,13 @@
+package test.javavitualthreadstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVitualThreadsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaVitualThreadsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/javavitualthreadstest/JavaVitualThreadsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.javavitualthreadstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVitualThreadsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
